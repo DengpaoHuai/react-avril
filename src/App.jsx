f553c621eae99c46e07f00c9b284d1957c7642e6
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-04-24 12:15:51 +0200

    correction card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import CustomCard from "./components/CustomCard";
 import CustomTitle from "./components/CustomTitle";
 
 function App() {
@@ -10,6 +11,10 @@ function App() {
       <CustomTitle title={title} subtitle={subtitle}>
         <button>démarrer le tuto</button>
       </CustomTitle>
+      <CustomCard title="Card 1" footerText="Ceci est le pied de la carte 1">
+        <h2>Card 1</h2>
+        <p>Contenu de la carte 1</p>
+      </CustomCard>
     </>
   );
 }
---
+++ src/components/CustomCard.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+const CustomCard = ({ title, footerText, children }) => {
+  return (
+    <>
+      <header>
+        <h1>{title}</h1>
+      </header>
+      <main>{children}</main>
+      <footer>
+        <p>{footerText}</p>
+      </footer>
+    </>
+  );
+};
+
+CustomCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  footerText: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export default CustomCard;

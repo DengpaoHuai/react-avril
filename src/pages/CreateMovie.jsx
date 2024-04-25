78440c5640a801200e9bd9b2e5ebad3d0bc74ffc
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-04-25 11:49:05 +0200

    add correction étape 1, 2, 3, 4

--- src/pages/CreateMovie.jsx
+++ src/pages/CreateMovie.jsx
@@ -1,10 +1,20 @@
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateMovie() {
   const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
   const maSuperbeFunctionDeSubmit = (data) => {
-    console.log(data);
+    fetch("https://crudcrud.com/api/046f91d286c8425c94758290f8abbfae/movies", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(() => {
+      navigate("/list_movies");
+    });
   };
 
   return (
